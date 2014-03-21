ce2ed9c2fd60242e5d20c6432561473a27e75f77
Emiliano Berenbaum <emiliano@Emilianos-MacBook-Pro.local>
2014-03-21 15:16:01 -0700

    debugging packer build

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # packer.chef cookbook
 
+Create a small example of an Packer generated AMI, configured via Chef.
+Deployed to EC2 using CloudFormation
+
 # Requirements
 
 # Usage
@@ -7,7 +10,10 @@
 # Attributes
 
 # Recipes
+default
+webserver
+database
 
 # Author
 
-Author:: YOUR_NAME (<YOUR_EMAIL>)
+Author:: Emiliano Berenbaum ( emiliano@medlista.com )
--- packer_build.sh
+++ packer_build.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 
-rm -r vendor
+# rm -r vendor
 
-bundle exec berks install --path vendor/cookbooks
+# bundle exec berks install --path vendor/cookbooks
 
-packer build \
-	-var "account_id=${AWS_ACCOUNT_ID}" \
-	-var "aws_acces_key_id=${AWS_ACCESS_KEY_ID}" \
+packer build -debug \
+	-var "aws_account_id=${AWS_ACCOUNT_ID}" \
+	-var "aws_access_key_id=${AWS_ACCESS_KEY_ID}" \
 	-var "aws_secret_key=${AWS_SECRET_ACCESS_KEY}" \
 	-var "aws_region=${AWS_REGION}" \
-	-var "aws_ami=${AWS_AMI}" \
-	-var "aws_groups=${AWS_GROUPS}" 
+	-var "aws_source_ami=${AWS_AMI}" \
+	-var "aws_security_group_id=${AWS_SECURITY_GROUP_ID}" \
 	-var "aws_instance_type=${AWS_INSTANCE_TYPE}" \
-	-var "x509_cert_path=${AWS_X509_CERT_PATH}" \
-	-var "x509_key_path=${AWS_X509_KEY_PATH}"
-
-	-only=amazon-instance packer.amazon-ebs.json
+	-only=amazon-ebs packer.amazon-ebs.json
